4f1bf99f99aa2a161438ad4a2ab91b2593810f65
gualberto7 <gualbertocuizav@gmail.com>
2023-02-23 15:10:00 -0400

    Header component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material";
+import HeaderApp from "./components/header/header";
 import BottomNav from "./components/nav/botton-nav";
 
 function App() {
   return (
     <div>
+      <HeaderApp />
       <h2>hello</h2>
       <Button variant="contained">Click here</Button>
       <BottomNav />
