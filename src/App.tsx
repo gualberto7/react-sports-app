a1a2ff2e16f01a59750659ac9a03ab41deda5826
gualberto7 <gualbertocuizav@gmail.com>
2023-02-23 14:21:56 -0400

    botton nav added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
+import BottomNav from "./components/nav/botton-nav";
 
 function App() {
   return (
     <div>
       <h2>hello</h2>
       <Button variant="contained">Click here</Button>
+      <BottomNav />
     </div>
   );
 }
